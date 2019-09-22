b9e94520b28e0808cbab2a44e2c49ab5c570101c
ruohan <1534007973@qq.com>
2019-09-22 16:59:43 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    home.html
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    login.html
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    // components: {
+    //   Login
+    // }
+  }
+</script>
