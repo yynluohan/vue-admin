e5a76661221f3c5f3eb55ffc63c53130028b6cb1
ruohan <1534007973@qq.com>
2019-09-25 23:41:19 +0800

    菜单完善

---
+++ src/static/menuData.js
@@ -0,0 +1,48 @@
+export const menuList = [
+  {
+    name: '首页',
+    icon: 'tag',
+    path: '/',
+  },
+  {
+    name: '产品管理',
+    icon: 'tag',
+    items: [
+      {
+        path: '/product/categroy',
+        name: '产品类别',
+        icon: 'tags',
+      },
+      {
+        path: '/product',
+        name: '产品',
+        icon: 'tags',
+      },
+      {
+        path: '/product/tag',
+        name: '产品标签',
+        icon: 'tags',
+      },
+      {
+        path: '/product/trial',
+        name: '试用装管理',
+        icon: 'tags',
+      },
+      {
+        path: '/product/evaluate',
+        name: '评价管理',
+        icon: 'tags',
+      },
+      {
+        path: '/product/freight',
+        name: '运费模板',
+        icon: 'tags',
+      },
+      {
+        path: '/product/brand',
+        name: '品牌管理',
+        icon: 'tags',
+      }
+    ]
+  }
+]
